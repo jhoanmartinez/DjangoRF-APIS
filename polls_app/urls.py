026b355e2d82b8f16fecc0a5e4900694301cb8a2
jhoanmartinez <dsa.jhoan@gmail.com>
2021-03-03 17:27:23 -0500

    APIView + serializer

--- polls_app/urls.py
+++ polls_app/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from . import views 
+from . import views
+from .apiviews import *
 
 urlpatterns = [
     path("", views.polls_home, name="homepage"),
-    path("polls/", views.polls_list, name="poll-list"), #GETs list of Poll
-    path("polls/<int:pk>/", views.polls_detail, name="polls-detail"), #GETs data of a specific Poll
+    # path("polls/", views.polls_list, name="poll-list"), #GETs list of Poll
+    # path("polls/<int:pk>/", views.polls_detail, name="polls-detail"), #GETs data of a specific Poll
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
 ]
 
 
--- polls_app/views.py
+++ polls_app/views.py
@@ -12,27 +12,27 @@ def polls_home(request):
     return JsonResponse(welcome)
 
 
-def polls_list(request):
-    MAX_OBJECTS = 20
-    polls = Poll.objects.all()[:MAX_OBJECTS]
-    data = {
-        "results": list
-                        (polls.values(
-                            #mismos nombres del modelo
-                            "id",
-                            "question", 
-                            "created_by",
-                            "pub_date")
-                        )
-            }
-    return JsonResponse(data)
+# def polls_list(request):
+#     MAX_OBJECTS = 20
+#     polls = Poll.objects.all()[:MAX_OBJECTS]
+#     data = {
+#         "results": list
+#                         (polls.values(
+#                             #mismos nombres del modelo
+#                             "id",
+#                             "question", 
+#                             "created_by",
+#                             "pub_date")
+#                         )
+#             }
+#     return JsonResponse(data)
 
-def polls_detail(request, pk):
-    poll = get_object_or_404(Poll, pk=pk)
-    data =  {"results": {
-                        "id":pk,
-                        "question":poll.question,
-                        "created_by":poll.created_by.username,
-                        "pub_date":poll.pub_date
-                        }}
-    return JsonResponse(data)
+# def polls_detail(request, pk):
+#     poll = get_object_or_404(Poll, pk=pk)
+#     data =  {"results": {
+#                         "id":pk,
+#                         "question":poll.question,
+#                         "created_by":poll.created_by.username,
+#                         "pub_date":poll.pub_date
+#                         }}
+#     return JsonResponse(data)

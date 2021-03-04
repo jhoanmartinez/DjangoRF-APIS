30e58b61d26690ccc2cd6dfa7ac2165fba8fa5b2
jhoanmartinez <dsa.jhoan@gmail.com>
2021-03-03 19:00:55 -0500

    generics APIView

--- polls_app/urls.py
+++ polls_app/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     # path("polls/", views.polls_list, name="poll-list"), #GETs list of Poll
     # path("polls/<int:pk>/", views.polls_detail, name="polls-detail"), #GETs data of a specific Poll
     path("polls/", PollList.as_view(), name="polls_list"),
-    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("choices/", ChoiceList.as_view(), name="choice_list"),
+    path("vote/", CreateVote.as_view(), name="create_vote"),
+
 ]
 
 

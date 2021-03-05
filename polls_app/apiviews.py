f0363533dc4a6d5d7c972ba4d9d977381b468daa
jhoanmartinez <dsa.jhoan@gmail.com>
2021-03-04 21:10:16 -0500

    url refactoring

--- polls_app/apiviews.py
+++ polls_app/apiviews.py
@@ -14,26 +14,42 @@ from rest_framework import generics
 #         return Response(data)
 
 # class PollDetail(APIView):
-
 #     def get(self, request, pk):
 #         poll = Poll.objects.get(pk=pk)
 #         data = PollSerializer(poll).data
 #         return Response(data)
 
+#Get and Post
 class PollList(generics.ListCreateAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
+#Get and delete
 class PollDetail(generics.RetrieveDestroyAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
+#List
 class ChoiceList(generics.ListCreateAPIView):
-    queryset = Choice.objects.all()
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
+        return queryset
     serializer_class = ChoiceSerializer
 
-class CreateVote(generics.CreateAPIView):
+#Post
+class CreateVote(APIView):
     serializer_class = VoteSerializer
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {'choice': choice_pk, 
+                'poll': pk, 
+                'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            vote = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
